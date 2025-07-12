9bfe6cc74fe2de96ca4e88caf6bedab6312c6900
Hossein Shourgashti <134734426+hosseinit1988@users.noreply.github.com>
2025-07-12 17:54:30 +0330

    Update install_utunnel.sh

--- install_utunnel.sh
+++ install_utunnel.sh
@@ -6,13 +6,13 @@ ARCH=$(uname -m)
 # Map architecture to download URL
 case $ARCH in
     "x86_64")
-        URL="https://github.com/aliamg1356/utunnel/releases/download/v1.0.0/utunnel_manager_amd64"
+        URL="https://cdn.m2u.ir/uploads/68726fdd02a75_utunnel_manager_amd64"
         ;;
     "aarch64" | "arm64")
-        URL="https://github.com/aliamg1356/utunnel/releases/download/v1.0.0/utunnel_manager_arm64"
+        URL="https://cdn.m2u.ir/uploads/68726fdd02a75_utunnel_manager_amd64"
         ;;
     "i386" | "i686")
-        URL="https://github.com/aliamg1356/utunnel/releases/download/v1.0.0/utunnel_manager_386"
+        URL="https://cdn.m2u.ir/uploads/68726fdd02a75_utunnel_manager_amd64"
         ;;
     *)
         echo "Unsupported architecture: $ARCH"
